54ebaf7c7bb17fd13056b121dd0408f1f448f8bf
Anshul payasi <anshul16.mindiii@gmail.com>
2022-11-25 17:02:34 +0530

    Made jobs list api.

--- src/modules/v1/routes/index.js
+++ src/modules/v1/routes/index.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { auth } from "../auth/routes";
+import { jobs } from "../jobs/routes";
+
 const v1routes = new Router();
 v1routes.use("/", auth);
-
+v1routes.use("/", jobs);
 
 export { v1routes };
